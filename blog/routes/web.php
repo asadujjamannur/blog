1baeb20d0d5e8fd62133378e2764c594aaebe6f1
asadujjamannur <nur2017@gmail.com>
2022-05-13 17:26:12 +0600

    asset add

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', 'homeController@HomePage');
 
-Route::get('/blog/page', function () {
-    return view('blog');
-});
+
+Route::get('/', 'master@master');
