d4b846fedd37d2b3d169eb7d1a6850d56c3ba65c
asadujjamannur <nur2017@gmail.com>
2022-05-13 17:30:12 +0600

    Update master.blade.php

--- blog/resources/views/frontend/master.blade.php
+++ blog/resources/views/frontend/master.blade.php
@@ -17,7 +17,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
         <!-- css file  -->
-        <link rel="stylesheet" href="{{asset('css/plugins.css')}}">
+        <link rel="stylesheet" href="{{asset('frontend/css/plugins.css')}}">
         <link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{asset('frontend/css/flaticon.css')}}">
         <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
